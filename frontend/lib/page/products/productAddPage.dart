3ae873058c9ceaffede902c8d8084aafa878e425
Nubdubi <kojun1412@naver.com>
2023-04-17 19:38:53 +0900

    vdvdvd

--- frontend/lib/page/products/productAddPage.dart
+++ frontend/lib/page/products/productAddPage.dart
@@ -11,19 +11,32 @@ class ProductAddPage extends StatefulWidget {
 class _ProductAddPageState extends State<ProductAddPage> {
   String result = '';
   TextEditingController barcodeinput = TextEditingController();
+  TextEditingController titleinput = TextEditingController();
+  TextEditingController exdateinput = TextEditingController();
+  TextEditingController placeinput = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
-            Container(
-                width: 500,
+            SizedBox(
+                width: 800,
                 child: Row(
                   children: [
-                    ElevatedButton(
-                      onPressed: () async {
+                    Text('바코드'),
+                    SizedBox(
+                      width: 300,
+                      child: TextFormField(
+                        controller: barcodeinput,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () async {
                         var res = await Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -36,16 +49,51 @@ class _ProductAddPageState extends State<ProductAddPage> {
                           }
                         });
                       },
-                      child: const Text('Open Scanner'),
+                      child: Icon(Icons.barcode_reader),
                     ),
                   ],
                 )),
-            TextFormField(
-              controller: barcodeinput,
-            )
+            InputSection(
+              name: '상품명',
+              controller: titleinput,
+            ),
+            InputSection(
+              name: '유통기한',
+              controller: exdateinput,
+            ),
+            InputSection(
+              name: '위치',
+              controller: placeinput,
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class InputSection extends StatelessWidget {
+  InputSection({super.key, required this.name, required this.controller});
+  String name;
+  TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(name),
+        SizedBox(
+          width: 10,
+        ),
+        SizedBox(
+          width: 300,
+          child: TextFormField(
+            controller: controller,
+          ),
+        ),
+      ],
     );
   }
 }
