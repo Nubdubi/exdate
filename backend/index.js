3ee6719da1865eef223a09919af17fa80d09ddf1
DESKTOP-QDDSANM\kim <qmdch1@naver.com>
2023-04-18 23:09:29 +0900

    feat : backend api 테스트, api url에 josn 오타 수정

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,15 @@ var express = require("express");
 var app = express();
 var apikey = "e5999307e832428b9a4e";
 // http://openapi.foodsafetykorea.go.kr/api/인증키/서비스명/요청파일타입/요청시작위치/요청종료위치
-var openapiurl = ` http://openapi.foodsafetykorea.go.kr/api/${apikey}/C005/josn/1/10`;
+var openapiurl = `http://openapi.foodsafetykorea.go.kr/api/${apikey}/C005/json/1/1`;
 
-// var connection = mysql.createConnction({
-//   host:,
-//   user:,
-//   password:,
-//   database:,
-// });
-
-// connection.connect();
+app.use(express.json());
+app.use(express.urlencoded( {extended : false } ));
+app.listen(9000, (err) => {
+  console.log("http://localhost:9000/");
+});
 
 app.get("/", (req, res) => {
-  var userHeader = req.headers["user-header"];
-  console.log(userHeader);
-  if (userHeader === undefined) {
-    return res.json({ key: "ERR" });
-  }
-
   res.json([
     {
       key: "Value",
@@ -37,10 +28,28 @@ app.get("/", (req, res) => {
   ]);
 });
 
-// app.get("/test", (req, res) => {
-//   connection.query('SELECT * FROM author',(err,result,filed)=> {return res.json(result);})
-// });
+app.post("/product", (req, res) => {
+  const config = {
+    method: "post"
+  };
+  console.log(openapiurl+"/BAR_CD="+req.body.BAR_CD);
+  fetch(openapiurl+"/BAR_CD="+req.body.BAR_CD, config)
+  .then((response) => {
+    console.log(JSON.stringify(response));
+    res.json(response);
+  })
+  .catch((error) => res.send(error));
+  
 
-app.listen(9000, (err) => {
-  console.log("http://localhost:9000/");
+  // res.json([
+  //   { SITE_ADDR: "Value",},
+  //   { PRDLST_NM: "Value",},
+  //   { BAR_CD: "Value",},
+  //   { POG_DAYCNT: "Value",},
+  //   { PRDLST_DCNM: "Value",},
+  //   { BSSH_NM: "Value",},
+  //   { INDUTY_NM: "Value",},
+  // ]);
 });
+
+
