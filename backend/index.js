ab61365eeca8d019f2ead03d881447100a7bf8bf
Nubdubi <kojun1412@naver.com>
2023-04-24 09:56:07 +0900

    backend front테스트 페이지 연결

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,19 @@
-const cors = require('cors');
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const maria = require('./maria.js');
-
+const maria = require("./maria.js");
+var path = require("path");
 /**
  * express 설정
  */
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded( {extended : false } ));
+app.use(express.urlencoded({ extended: false }));
 app.listen(9000, (err) => {
   console.log("http://localhost:9000/");
 });
@@ -21,7 +23,10 @@ app.listen(9000, (err) => {
  */
 const apikey = "e5999307e832428b9a4e";
 // http://openapi.foodsafetykorea.go.kr/api/인증키/서비스명/요청파일타입/요청시작위치/요청종료위치
-const openapiurl = `http://openapi.foodsafetykorea.go.kr/api/${apikey}/C005/json/1/1`;
-
+const openapiurl = `http://openapi.foodsafetykoea.go.kr/api/${apikey}/C005/json/1/1`;
+app.use(express.static(path.join(__dirname, "../frontend/build/web")));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/web/index.html"));
+});
 app.use("/bucket", require("./router/bucket.js"));
 app.use("/product", require("./router/product.js"));
