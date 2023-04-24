08150bc6f5c68692dd18adc4b67d887eafc1f2eb
Nubdubi <kojun1412@naver.com>
2023-04-24 18:15:56 +0900

    googleauth

--- frontend/lib/page/auth/loginpage.dart
+++ frontend/lib/page/auth/loginpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/services/authService.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -7,6 +8,18 @@ class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
+
+const List<String> scopes = <String>[
+  'email',
+  'https://www.googleapis.com/auth/contacts.readonly',
+];
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  clientId:
+      '317921682520-osl28bu3k92j0st1hjm9ehvc8m9n29jl.apps.googleusercontent.com',
+  scopes: scopes,
+);
 
 class _LoginPageState extends State<LoginPage> {
   AuthService authService = AuthService();
