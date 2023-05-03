bad13591ae2be616464fc360360f352d2796d4f6
pskim <qmdch1@naver.com>
2023-05-03 15:34:34 +0900

    feat : https 설정 추가

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,38 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const maria = require("./maria.js");
+const https = require("https");
+const fs = require("fs");
+
 var path = require("path");
-/**
- * express 설정
- */
+
+
 app.use(
   cors({
     origin: true,
     credentials: true,
   })
 );
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.listen(9000, (err) => {
-  console.log("http://localhost:9000/");
+
+const port = 9001;
+const httpsPort = 9000;
+
+app.listen(port, (err) => {
+  console.log(`http://localhost:${port}/`);
 });
+
+//https 설정
+const options = {
+  key: fs.readFileSync("./keys/privkey.pem"),
+  cert: fs.readFileSync("./keys/fullchain.pem"),
+};
+https.createServer(options, app).listen(httpsPort, () => {
+  console.log(`https port ${httpsPort}`);
+});
+
 
 /**
  * 데이터 설정
